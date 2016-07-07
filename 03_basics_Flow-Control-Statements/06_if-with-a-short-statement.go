17167775dbda80c82a2162ba381c61583fffb913
Soichiro75 <yumemirutikara75@gmail.com>
2016-07-07 15:22:29 +0900

    Create Some GoCode

---
+++ 03_basics_Flow-Control-Statements/06_if-with-a-short-statement.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	}
+	return lim
+}
+
+func main() {
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20),
+	)
+}
+
+//=> 9 20
+//Why "9 20"? I think "9 10 20"......
